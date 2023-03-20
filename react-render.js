9e40ab53508d92dd0346a7c5d89d480e68f05047
sephpw00005574 <koushil.mankali@sephora.com>
2023-03-20 10:30:53 +0530

    react-render question done

--- react-render.js
+++ react-render.js
@@ -1,2 +1,15 @@
-sample;
-("sanple");
+/**
+ * Question => For a recursive description of DOM elements i.e,
+ *
+ *  CODE STARTS
+ *
+ *  const dom = {
+ *      type: "div",
+ *      props: {id: "hello"},
+ *      children: [{type: "h1", children: "hello"}]
+ *  }
+ *
+ *  CODE ENDS
+ *
+ * Prepare a function that prepares a actual dom elements.
+ */
